84f12a3e13508275886b8ac41ecec9e2a884a9f7
NickySlicksHaha <nickengmann_2@yahoo.com>
2017-06-11 22:46:18 -0500

    routing of all listing pages

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 //initialize firebase
 var firebase = require("firebase");
 // require('firebase/messaging');
-// var admin = require("firebase-admin");
-// var serverKey = require("/public/que.json");
+
 
 var config = {
   apiKey: "AIzaSyCZ6rmPq93DjrPafTaWayIq-adXR2QtLlg",
@@ -15,10 +14,7 @@ var config = {
 };
 
 firebase.initializeApp(config);
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://firststepin-1bd8e.firebaseio.com"
-// });
+
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -28,6 +24,24 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 var provider = new firebase.auth.GoogleAuthProvider();
 // Set view engine to EJS and locate views
+var urlSheets =
+{
+  "ListingName":
+  {
+    "Food Banks":"https://docs.google.com/spreadsheets/d/18C24pEmFTz9JhIX_8UUhHEb5GenBPtrW_6WPMP_muzM/edit#gid=796165972",
+    "Restaurants":"https://docs.google.com/spreadsheets/d/18C24pEmFTz9JhIX_8UUhHEb5GenBPtrW_6WPMP_muzM/edit#gid=0",
+    "International Markets":"https://docs.google.com/spreadsheets/d/18C24pEmFTz9JhIX_8UUhHEb5GenBPtrW_6WPMP_muzM/edit#gid=634490059",
+    "Farmers Markets":"https://docs.google.com/spreadsheets/d/18C24pEmFTz9JhIX_8UUhHEb5GenBPtrW_6WPMP_muzM/edit#gid=634490059",
+    "Rent Assistance":"https://docs.google.com/spreadsheets/d/1Lv2hYUkQSsmumUKOYrXOXYKyKG2qQ0f-IWx5PbmYI1o/edit#gid=0",
+    "Affordable Housing":"https://docs.google.com/spreadsheets/d/1Lv2hYUkQSsmumUKOYrXOXYKyKG2qQ0f-IWx5PbmYI1o/edit#gid=876982746",
+    "Disputes":"https://docs.google.com/spreadsheets/d/1Lv2hYUkQSsmumUKOYrXOXYKyKG2qQ0f-IWx5PbmYI1o/edit#gid=884749198",
+    "ESL Classes":"https://docs.google.com/spreadsheets/d/1-Mkj3ifbDBnwvbOsbG8iRyxO_Mj40pNdP28ahoZMOQA/edit#gid=0",
+    "General Education":"https://docs.google.com/spreadsheets/d/1-Mkj3ifbDBnwvbOsbG8iRyxO_Mj40pNdP28ahoZMOQA/edit#gid=1712191606",
+    "K-12":"https://docs.google.com/spreadsheets/d/1-Mkj3ifbDBnwvbOsbG8iRyxO_Mj40pNdP28ahoZMOQA/edit#gid=262570677",
+    "Mosques And Churches": "https://docs.google.com/spreadsheets/d/1m1qsgFgL6W5XbPnKQ_ljFWMRWq7cVhe_gKN66qMvJf8/edit#gid=0",
+    "Groups": "https://docs.google.com/spreadsheets/d/1m1qsgFgL6W5XbPnKQ_ljFWMRWq7cVhe_gKN66qMvJf8/edit#gid=667001242"
+  }
+}
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
@@ -41,10 +55,14 @@ app.get('/signin', (request, response) => {
   response.render('signin'); 
 });
 app.get('/food', (request, response) => {
-  response.render('food'); 
-});
-app.get('/housing', (request, response) => {
-  response.render('housing'); 
+  response.render('food');
+ });
+app.get('/:listing(Food%20Banks|Restaurants|International%20Markets|Farmers%20Markets|Rent%20Assistance|Affordable%20Housing|Disputes|ESL%20Classes|General%20Education|K-12|Mosques%20And%20Churches|Groups)?', (request, response) => {
+  // This is where I find the specific listing data that I want
+  var name = request.params.listing; 
+  console.log(name);
+  var urlSpecific=urlSheets.ListingName[name];
+  response.render('listing', {ListingName:name, urlSheetrock:urlSpecific});
 });
 app.get('/transportation', (request, response) => {
   response.render('transportation'); 
